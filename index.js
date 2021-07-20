a3aaff7fa3424e3db167a1b12459425e6271f91f
ashokav008 <ashokwavre.wce@gamil.com>
2021-07-20 16:07:09 +0530

    edited the scrpt file

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log("this is form validation using regular expression in javascript");
 const name = document.getElementById("name");
 const email = document.getElementById("email");
 const mono = document.getElementById("mono");
@@ -6,15 +5,7 @@ const terms = document.getElementById("terms");
 let nameValid = false;
 let emailValid = false;
 let monoValid = false;
-// ------------------------------------------------------------?
-// ------------------------------------------------------------?
 let submit = document.getElementById('submit');
-
-
-
-
-// ---------------------------------------------------------------------
-// ---------------------------------------------------------------------
 
 
 
@@ -22,7 +13,7 @@ name.addEventListener('blur', () => {
     let regex_name = /^[a-zA-Z]([0-9a-zA-Z]){5,15}/
     let str_name = name.value;
     if (regex_name.test(str_name)) {
-        console.log("It matched")
+        // console.log("It matched")
         name.classList.remove('is-invalid');
         nameValid = true;
 
@@ -40,12 +31,11 @@ email.addEventListener('blur', () => {
     let regex_email = /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/
     let str_email = email.value;
     if (regex_email.test(str_email)) {
-        console.log("It matched")
         email.classList.remove('is-invalid');
         emailValid = true;
 
     } else {
-        console.log("it not Matched");
+
         email.classList.add('is-invalid');
         emailValid = false;
     }
@@ -58,12 +48,10 @@ mono.addEventListener('blur', () => {
     let str_Mono = mono.value;
     if (regex_Mono.test(str_Mono)) {
         mono.classList.remove('is-invalid');
-        console.log("It matched")
         monoValid = true;
 
     } else {
         mono.classList.add('is-invalid')
-        console.log("it not Matched");
         monoValid = false;
     }
     allCorrect()
@@ -74,11 +62,8 @@ mono.addEventListener('blur', () => {
 function allCorrect() {
     if (monoValid && emailValid && nameValid) {
         submit.classList.remove('disabled');
-        console.log("remove the attriubutes of submit")
     } else {
-        // if (monoValid != true && emailValid != true && nameValid != true) {
         submit.classList.add('disabled');
-        console.log("add the attriubutes of submit")
     }
 }
 
